8c43bf484c5955c906d8df37963e4ef841266c57
Leszek Andrukanis <andrukanisleszek@gmail.com>
2017-10-09 23:19:10 +0200

    Minor fix.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'csv'
 
 
 PROJECT_ROOT = "#{Dir.pwd}"
-CSV_PATH     = "#{PROJECT_ROOT}/csv/"
+CSV_PATH     = "#{PROJECT_ROOT}/csv"
 JIRA_CREDENTIALS  =  YAML.load_file("#{PROJECT_ROOT}/jira_credentials.yml")
 
 Dir["#{PROJECT_ROOT}/models/*.rb"].each {|file| require file }
